7ccdb75cffc5f644d693467de0f3f04fdb93b4da
JaniceFy <janicen.fy@gmail.com>
2024-05-04 23:27:09 -0600

    Subiendo los archivos

---
+++ encabezado.php
@@ -0,0 +1,3 @@
+<h2 id="centrado">Control de productos</h2>
+<img src="img/imagen1.jpg" width="600" height="100" /> <!-- Cambio de atributo -->
+<!-- <h4 id="centrado">Registro del nuevo producto</h4> -->
